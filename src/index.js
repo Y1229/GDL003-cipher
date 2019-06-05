c63935eb9d1225310ff20567b7c34b839b2678b2
Y1229 <yos_hdz.pi@hotmail.com>
2019-06-05 15:31:28 -0500

    Se agrego eventos de onclick

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-let string=document.getElementById('message');
+//Ocultar Div
+
+//funcionamiento del string
+let string=document.getElementById('message').value;document.getElementById('message').innerHTML="Tu mensaje es: "+ string;
+
+enCode.addEventListener("onclick",function{
+console.log("hola");
+});
+deCode.addEventListener("onclick",function{
+console.log("bye");
+});
